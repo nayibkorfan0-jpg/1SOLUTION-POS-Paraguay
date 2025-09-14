ec66e055dc7ca927ce959b941ab844988682839d
nayibkorfan0-jpg <nayibkorfan0@gmail.com>
2025-09-14 00:24:32 -0300

    Add client/src/components/examples/Dashboard.tsx

---
+++ client/src/components/examples/Dashboard.tsx
@@ -0,0 +1,11 @@
+import { Dashboard } from '../Dashboard';
+
+export default function DashboardExample() {
+  return (
+    <div className="min-h-screen bg-background">
+      <div className="container mx-auto p-6">
+        <Dashboard />
+      </div>
+    </div>
+  );
+}
